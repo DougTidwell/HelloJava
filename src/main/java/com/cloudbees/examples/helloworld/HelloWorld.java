15804c4ae2760e5b125953b9fc0a7e3d2bc938e5
Doug Tidwell <doug@dougtidwell.com>
2019-12-31 14:08:37 -0500

    Minor changes

--- src/main/java/com/cloudbees/examples/helloworld/HelloWorld.java
+++ src/main/java/com/cloudbees/examples/helloworld/HelloWorld.java
@@ -2,6 +2,6 @@ package com.cloudbees.examples.helloworld;
 
 public class HelloWorld {
   public static void main(String args[]){
-    System.out.println("Hello World!");
+    System.out.println("Hello Jenkins World!");
   }
 }
